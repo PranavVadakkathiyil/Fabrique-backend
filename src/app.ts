560c72f21bbea514215132490791cefcf5b2abb3
PranavVadakkathiyil <vpranav8123@gmail.com>
2025-07-24 14:22:06 +0530

    origin change

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import passport from "passport";
 const app:Application = express()
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: "*",
   credentials: true,
 }));
 app.use(express.json({limit:"16kb"}))
